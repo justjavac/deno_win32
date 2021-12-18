bb83af17925a9a9e8c47ea89a4911c7e181a3734
justjavac <justjavac@gmail.com>
2021-12-18 22:01:18 +0800

    add modality dialog box

--- user32/MessageBox.ts
+++ user32/MessageBox.ts
@@ -136,7 +136,36 @@ export const MB_DEFBUTTON4 = 0x00000300;
 /// To indicate the modality of the dialog box, specify one of the following values.
 ///===================
 
-// TODO
+/**
+ * The user must respond to the message box before continuing work in the window
+ * identified by the hWnd parameter. However, the user can move to the windows of
+ * other threads and work in those windows.
+ * 
+ * Depending on the hierarchy of windows in the application, the user may be able
+ * to move to other windows within the thread. All child windows of the parent of
+ * the message box are automatically disabled, but pop-up windows are not.
+ * 
+ * `MB_APPLMODAL` is the default if neither `MB_SYSTEMMODAL` nor `MB_TASKMODAL` is specified.
+ */
+export const MB_APPLMODAL = 0x00000000;
+
+/**
+ * Same as **MB_APPLMODAL** except that the message box has the **WS_EX_TOPMOST** style.
+ * Use system-modal message boxes to notify the user of serious, potentially damaging
+ * errors that require immediate attention (for example, running out of memory). 
+ * This flag has no effect on the user's ability to interact with windows other than
+ * those associated with `hWnd`.
+ */
+export const MB_SYSTEMMODAL = 0x00001000;
+
+/**
+ * Same as `MB_APPLMODAL` except that all the top-level windows belonging to the
+ * current thread are disabled if the hWnd parameter is `NULL`. 
+ * Use this flag when the calling application or library does not have a window
+ * handle available but still needs to prevent input to other windows in the
+ * calling thread without suspending other threads.
+ */
+export const MB_TASKMODAL = 0x00002000;
 
 /// ==================
 /// To specify other options, use one or more of the following values.
