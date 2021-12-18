835cd3688e45d815de1fd12e8a06c5df456a24a3
justjavac <justjavac@gmail.com>
2021-12-18 22:08:23 +0800

    add more messagebox options

--- user32/MessageBox.ts
+++ user32/MessageBox.ts
@@ -140,11 +140,11 @@ export const MB_DEFBUTTON4 = 0x00000300;
  * The user must respond to the message box before continuing work in the window
  * identified by the hWnd parameter. However, the user can move to the windows of
  * other threads and work in those windows.
- * 
+ *
  * Depending on the hierarchy of windows in the application, the user may be able
  * to move to other windows within the thread. All child windows of the parent of
  * the message box are automatically disabled, but pop-up windows are not.
- * 
+ *
  * `MB_APPLMODAL` is the default if neither `MB_SYSTEMMODAL` nor `MB_TASKMODAL` is specified.
  */
 export const MB_APPLMODAL = 0x00000000;
@@ -152,7 +152,7 @@ export const MB_APPLMODAL = 0x00000000;
 /**
  * Same as **MB_APPLMODAL** except that the message box has the **WS_EX_TOPMOST** style.
  * Use system-modal message boxes to notify the user of serious, potentially damaging
- * errors that require immediate attention (for example, running out of memory). 
+ * errors that require immediate attention (for example, running out of memory).
  * This flag has no effect on the user's ability to interact with windows other than
  * those associated with `hWnd`.
  */
@@ -160,7 +160,7 @@ export const MB_SYSTEMMODAL = 0x00001000;
 
 /**
  * Same as `MB_APPLMODAL` except that all the top-level windows belonging to the
- * current thread are disabled if the hWnd parameter is `NULL`. 
+ * current thread are disabled if the hWnd parameter is `NULL`.
  * Use this flag when the calling application or library does not have a window
  * handle available but still needs to prevent input to other windows in the
  * calling thread without suspending other threads.
@@ -171,7 +171,58 @@ export const MB_TASKMODAL = 0x00002000;
 /// To specify other options, use one or more of the following values.
 ///===================
 
-// TODO
+/**
+ * Same as desktop of the interactive window station. For more information,
+ * see [Window Stations][1].
+ *
+ * [1]: https://docs.microsoft.com/en-us/windows/desktop/winstation/window-stations
+ *
+ * If the current input desktop is not the default desktop, **MessageBox** does
+ * not return until the user switches to the default desktop.
+ */
+export const MB_DEFAULT_DESKTOP_ONLY = 0x00020000;
+
+/** The text is right-justified. */
+export const MB_RIGHT = 0x00080000;
+
+/**
+ * Displays message and caption text using right-to-left reading order on
+ * Hebrew and Arabic systems.
+ */
+export const MB_RTLREADING = 0x00100000;
+
+/**
+ * The message box becomes the foreground window. Internally, the system
+ * calls the [SetForegroundWindow][1] function for the message box.
+ *
+ * [1]: https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-setforegroundwindow
+ */
+export const MB_SETFOREGROUND = 0x00010000;
+
+/** The message box is created with the `WS_EX_TOPMOST` window style. */
+export const MB_TOPMOST = 0x00040000;
+
+/**
+ * The caller is a service notifying the user of an event. The function
+ * displays a message box on the current active desktop, even if there is no
+ * user logged on to the computer.
+ *
+ * **Terminal Services**: If the calling thread has an impersonation token,
+ * the function directs the message box to the session specified in the
+ * impersonation token.
+ *
+ * If this flag is set, the `hWnd` parameter must be `NULL`. This is so that
+ * the message box can appear on a desktop other than the desktop corresponding
+ * to the `hWnd`.
+ *
+ * For information on security considerations in regard to using this flag, see
+ * [Interactive Services][1]. In particular, be aware that this flag can produce
+ * interactive content on a locked desktop and should therefore be used for only
+ * a very limited set of scenarios, such as resource exhaustion.
+ *
+ * [1]: https://docs.microsoft.com/en-us/windows/desktop/Services/interactive-services
+ */
+export const MB_SERVICE_NOTIFICATION = 0x00200000;
 
 /// ==================
 /// Return value
