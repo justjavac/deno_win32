00617ebae7b29d11566864340a1055133b8eb50a
迷渡 <justjavac@gmail.com>
2022-07-29 20:55:37 +0800

    use `null` instead of `0n` (#5)

--- examples/MessageBox.ts
+++ examples/MessageBox.ts
@@ -1,7 +1,7 @@
 import * as win32 from "../mod.ts";
 
 const msgboxID = win32.MessageBox(
-  0n,
+  null,
   "Hello World\n你好，世界\nこんにちは世界\nBonjour le monde\nمرحبا بالعالم",
   "Deno FFI",
   win32.MB_YESNO | win32.MB_ICONWARNING,
